95bb94bcb725ee9218609b3cc8a4dc0884da9fbb
tomasjakub <tomasjakub21@gmail.com>
2023-02-22 19:08:40 +0100

    update stream component

--- src/app/components/stream/stream.component.ts
+++ src/app/components/stream/stream.component.ts
@@ -7,16 +7,15 @@ import { interval, Subject, Subscription } from 'rxjs';
 @Component({
   selector: 'app-stream',
   templateUrl: './stream.component.html',
-  styleUrls: ['./stream.component.css']
+  styleUrls: ['./stream.component.css'],
 })
 export class StreamComponent implements OnInit, OnDestroy {
-
-  url = 'https://dog.ceo/api/breeds/list/all'
+  url = 'https://dog.ceo/api/breeds/list/all';
   object: Object | undefined;
 
   destroy$ = new Subject<boolean>();
   subscription: Subscription | undefined;
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     // this.getWithInterval()
@@ -25,39 +24,45 @@ export class StreamComponent implements OnInit, OnDestroy {
 
   //first call
   getCall() {
-    this.http.get(this.url).pipe(
-      tap((value) => { console.log(value); })
-    ).subscribe()
+    this.http
+      .get(this.url)
+      .pipe(
+        tap((value) => {
+          console.log(value);
+        })
+      )
+      .subscribe();
   }
 
   // without unsubscribe
 
   getWithInterval() {
-    interval(10000).pipe(
-      startWith(0),
-      mergeMap(() => this.http.get(this.url)),
-      tap((value) => { console.log(value); })
-    ).subscribe()
+    interval(10000)
+      .pipe(
+        startWith(0),
+        mergeMap(() => this.http.get(this.url)),
+        tap((value) => {
+          console.log(value);
+        })
+      )
+      .subscribe();
   }
 
-
   getWithIntervalUnsubscribe() {
-    this.subscription = interval(10000).pipe(
-      startWith(0),
-      mergeMap(() => this.http.get(this.url)),
-      tap((value) => {
-        this.object = value;
-        console.log(value);
-      }),
-      takeUntil(this.destroy$)
-    ).subscribe()
+    this.subscription = interval(10000)
+      .pipe(
+        startWith(0),
+        mergeMap(() => this.http.get(this.url)),
+        tap((value) => {
+          this.object = value;
+          console.log(value);
+        }),
+        takeUntil(this.destroy$)
+      )
+      .subscribe();
   }
 
   ngOnDestroy(): void {
     this.subscription?.unsubscribe();
   }
-
-
-
-
 }
