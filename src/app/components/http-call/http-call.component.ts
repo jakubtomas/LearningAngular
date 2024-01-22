db3b6786e8270b4d719ca425599a11562864ed4e
tomasjakub <tomasjakub21@gmail.com>
2024-01-22 16:58:31 +0100

    create proxy for CORS example

--- src/app/components/http-call/http-call.component.ts
+++ src/app/components/http-call/http-call.component.ts
@@ -1,6 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
+export interface WebFormData {
+  html: string;
+  html_minimal: string;
+  js: string;
+  link: string;
+}
 @Component({
   selector: 'app-http-call',
   templateUrl: './http-call.component.html',
@@ -8,7 +14,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HttpCallComponent implements OnInit {
 
-  apiData = null;
+  apiData:WebFormData = {
+    html: '',
+    html_minimal: '',
+    js: '',
+    link: ''
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -19,13 +30,13 @@ export class HttpCallComponent implements OnInit {
   private getData(){
 
 
+    this.http.get('web-form-preview/code?a=89dd3ca4-60fd-11ee-8523-3cecef38fa8d&g=999pjdmoxv5uzhqm31b3apu3wijzofl9nhin1gc8vjyhf3q1tghue0vryzf3wmx0473gyu05tfjfiejrq9voic81oprqinc34r7zy')
+
     .subscribe({
       next: (data: any) => {
         console.log('Response:', data);
         // Handle the data as needed
         this.apiData= data
-        console.log('--------');
-
         console.log(data);
 
 
@@ -38,5 +49,14 @@ export class HttpCallComponent implements OnInit {
     });
   }
 
+
+  mockFunction(){
+    const object = {
+      "html":'hello',
+      "html_minimal":'hello',
+      "js":'hello',
+      "link":'hello'
+    }
+  }
 
 }
